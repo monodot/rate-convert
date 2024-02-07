7bd30e3c362ac9a2699de6d8744beb4355854e96
Tom Donohue <monodot@gmail.com>
2024-02-07 13:16:02 +0000

    Fixy buggy

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
   import Converter from './lib/Converter.svelte';
 </script>
 
